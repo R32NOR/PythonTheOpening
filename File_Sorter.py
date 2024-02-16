55c2016cdc676b20ba90f5f8d0449b3b29a5ebff
R32NOR <piotrulawisniewski@gmail.com>
2024-02-16 21:29:52 +0100

    update File Sorter
    code works- now I will just finish it to works better

--- File_Sorter.py
+++ File_Sorter.py
@@ -1,10 +1,9 @@
 import os
-import datetime
-from typing import List
+import shutil
 
 path=input('drop the directory path that you want to sort: ')
 
-
+# Function that returns list with all filenames from chosen dir
 def file_list_receiver(directory):
     # Initialize empty list to store the file names:
     fileList=[]
@@ -16,40 +15,71 @@ def file_list_receiver(directory):
             # add file to the list:
             fileList.append(fileName)
 
+    # As a return we get list of files in directory and are not a directories
     return fileList
 
-imageExtension= ['png', 'jpg', 'bmp', 'gif']
-pdfExtension= ['pdf']
-documentExtension= ['doc', 'docx', 'xsl', 'xslx', 'txt']
-musicExtension = ['mp3', 'wav']
-videoExtension = ['mp4']
 
-newDirectories={'imageExtension': 'Images', 'pdfExtension': 'PDFs', 'documentExtension': "Docs", 'musicExtension': 'Music', 'videoExtension': 'Video'}
+# function that returns which category file is:
+def file_category(fileExtension):
+
+    # declaring groups of file types
+    fileTypesDict = {
+        'Images': ['png', 'jpg', 'bmp', 'gif'],
+        'PDF': ['pdf'],
+        'Docs': ['doc', 'docx', 'xls', 'xlsx', 'txt', 'ppt', 'pptx', 'odt'],
+        'Music': ['mp3', 'wav'],
+        'Video': ['mp4'],
+        'Archive': ['rar', 'zip', '7z']
+    }
+
+    # loop that returns type of file
+    for filetype in fileTypesDict.values():
+        if fileExtension.lower() in filetype:
+            fileCategory = list(fileTypesDict.keys())[list(fileTypesDict.values()).index(filetype)]
+            return fileCategory
+        else:
+            continue
+    else:
+        fileCategory='Other'
+    return fileCategory
+
+#function that creates category directory (if it doesnot exist)
+def create_category_dir(directory, fileCategory):
+    finalDir=os.path.join(directory, fileCategory) # final directory for current file category
+
+    if os.path.exists(finalDir)==True:
+        return finalDir
+    else:
+        os.makedirs(finalDir)
 
 
-
+# below we set function that we shuld input path to the directory and list of files
 def file_sorter (directory, listOfFiles):
+    fileExt=''  #variable that will contain file extension retrieved in below loop
 
-    fileExtensions=[]
     # taking each file in list of files
     for file in listOfFiles:
-        # take extracted file list to get the file extension:
+        # below we are looping for retrieving of file extension
+         fileNameLength = len(file)
+         dotFromBack = 1
 
-        dotPosition=int(len(file)-1)
-        for dotPosition in range(0,len(file)):
-            if file[dotPosition] != '.':
-                dotPosition -= 1
-            else:
-                fileExt=file[dotPosition:]
-                fileExtensions.append(fileExt)
+         for dotFromBack in range(1,fileNameLength):
+             if file[dotFromBack*(-1)] != '.':
+                 dotFromBack += 1
+             else:
+                 dotPosition=fileNameLength-dotFromBack
+                 fileExt = file[dotPosition+1:]
+                 #print(file, fileExt)
+                 fileCategory=file_category(fileExt)
+                 create_category_dir(path,fileCategory)
+                 sourceFilePath=os.path.join(path,file)
+                 destinationDirectoryPath=os.path.join(path,fileCategory)
+                 shutil.move(sourceFilePath, destinationDirectoryPath) # replace current file to final directory
+                 break
 
-    return print(fileExtensions)
+# function call
+files=file_list_receiver(path) # list of file that is returned from f. file_list_receiver
 
-
-
-
-
-files=file_list_receiver(path)
 file_sorter(path, files)
 
 
